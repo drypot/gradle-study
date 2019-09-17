f7e423224305b3a502da3adf93d484a99be1ec0c
drypot <drypot@gmail.com>
2019-09-17 15:22:48 +0900

    add copy task.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 group = "com.drypot"
 version = "1.0-SNAPSHOT"
 
-java {                                      (4)
+java {
   sourceCompatibility = JavaVersion.VERSION_11
   targetCompatibility = JavaVersion.VERSION_11
 }
@@ -18,19 +18,34 @@ dependencies {
     testCompile("junit:junit:4.12")
 }
 
+tasks.create("helloWorldGradle") {
+  group = "Custom"
+  doLast {
+    println("Hello World Gradle !")
+  }
+}
 
-task("helloWorld", JavaExec::class) {
+tasks.create<JavaExec>("helloWorld") {
+  group = "Custom"
   main = "com.drypot.HelloWorld"
   classpath = sourceSets["main"].runtimeClasspath
 }
 
-task("dumpTimeZone", JavaExec::class) {
+tasks.create<JavaExec>("dumpTimeZone") {
+  group = "Custom"
   main = "com.drypot.DumpTimeZone"
   classpath = sourceSets["main"].runtimeClasspath
 }
 
-task("helloWorldGradle") {
-  doLast {
-    println("Hello World Gradle !")
-  }
+tasks.create<Copy>("copy") {
+  group = "Custom"
+  from("copy-test/src")
+  into("copy-test/dest")
+}
+
+tasks.create<Zip>("zip") {
+  group = "Custom"
+  archiveFileName.set("sample.zip")
+  destinationDirectory.set(file("copy-test/zip"))
+  from("copy-test/src")
 }
